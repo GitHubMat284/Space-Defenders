e5f22d7f07fcb74c29ed7f77bc611e7f7b5e8f4c
Matheo Villacres <mathius85203@gmail.com>
2024-12-26 01:50:16 -0600

    Basic Movement
    Work to move left to right our player and enemies spawn on random places

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,108 +29,111 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.label1 = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.Label();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.bullet = new System.Windows.Forms.PictureBox();
             this.enemyThree = new System.Windows.Forms.PictureBox();
             this.enemyTwo = new System.Windows.Forms.PictureBox();
             this.enemyOne = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.enemyThree)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.enemyTwo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.enemyOne)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // txtScore
             // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(0, 232);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(899, 193);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "0";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(60, 225);
+            this.txtScore.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(674, 157);
+            this.txtScore.TabIndex = 5;
+            this.txtScore.Text = "0";
+            this.txtScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // gameTimer
             // 
             this.gameTimer.Interval = 20;
             this.gameTimer.Tick += new System.EventHandler(this.mainGameTimerEvent);
             // 
-            // pictureBox5
+            // player
             // 
-            this.pictureBox5.Image = global::Space_Defenders.Properties.Resources.f35_Player1;
-            this.pictureBox5.Location = new System.Drawing.Point(383, 555);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(104, 150);
-            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox5.TabIndex = 4;
-            this.pictureBox5.TabStop = false;
-            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            this.player.Image = global::Space_Defenders.Properties.Resources.f35_Player;
+            this.player.Location = new System.Drawing.Point(345, 474);
+            this.player.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(104, 150);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 4;
+            this.player.TabStop = false;
+            this.player.Click += new System.EventHandler(this.player_Click);
             // 
-            // pictureBox4
+            // bullet
             // 
-            this.pictureBox4.Image = global::Space_Defenders.Properties.Resources.bullet;
-            this.pictureBox4.Location = new System.Drawing.Point(439, 473);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(7, 27);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox4.TabIndex = 3;
-            this.pictureBox4.TabStop = false;
-            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            this.bullet.Image = global::Space_Defenders.Properties.Resources.bullet;
+            this.bullet.Location = new System.Drawing.Point(393, 384);
+            this.bullet.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bullet.Name = "bullet";
+            this.bullet.Size = new System.Drawing.Size(7, 27);
+            this.bullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.bullet.TabIndex = 3;
+            this.bullet.TabStop = false;
             // 
             // enemyThree
             // 
             this.enemyThree.Image = global::Space_Defenders.Properties.Resources.enemy;
-            this.enemyThree.Location = new System.Drawing.Point(729, 44);
+            this.enemyThree.Location = new System.Drawing.Point(634, 36);
+            this.enemyThree.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.enemyThree.Name = "enemyThree";
             this.enemyThree.Size = new System.Drawing.Size(100, 85);
             this.enemyThree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.enemyThree.TabIndex = 2;
             this.enemyThree.TabStop = false;
-            this.enemyThree.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
             // enemyTwo
             // 
             this.enemyTwo.Image = global::Space_Defenders.Properties.Resources.enemy;
-            this.enemyTwo.Location = new System.Drawing.Point(383, 39);
+            this.enemyTwo.Location = new System.Drawing.Point(349, 36);
+            this.enemyTwo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.enemyTwo.Name = "enemyTwo";
             this.enemyTwo.Size = new System.Drawing.Size(100, 85);
             this.enemyTwo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.enemyTwo.TabIndex = 1;
             this.enemyTwo.TabStop = false;
-            this.enemyTwo.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // enemyOne
             // 
             this.enemyOne.Image = global::Space_Defenders.Properties.Resources.enemy;
-            this.enemyOne.Location = new System.Drawing.Point(63, 44);
+            this.enemyOne.Location = new System.Drawing.Point(47, 36);
+            this.enemyOne.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.enemyOne.Name = "enemyOne";
             this.enemyOne.Size = new System.Drawing.Size(100, 85);
             this.enemyOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.enemyOne.TabIndex = 0;
             this.enemyOne.TabStop = false;
-            this.enemyOne.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Cyan;
-            this.ClientSize = new System.Drawing.Size(900, 732);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox4);
+            this.ClientSize = new System.Drawing.Size(800, 674);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.bullet);
             this.Controls.Add(this.enemyThree);
             this.Controls.Add(this.enemyTwo);
             this.Controls.Add(this.enemyOne);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtScore);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Space Defenders";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bullet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.enemyThree)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.enemyTwo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.enemyOne)).EndInit();
@@ -144,9 +147,9 @@
         private System.Windows.Forms.PictureBox enemyOne;
         private System.Windows.Forms.PictureBox enemyTwo;
         private System.Windows.Forms.PictureBox enemyThree;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox bullet;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label txtScore;
         private System.Windows.Forms.Timer gameTimer;
     }
 }
